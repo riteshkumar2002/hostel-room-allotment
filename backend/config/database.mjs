9997a299ef6b0f5828ddb41bc00c995ba4cd3151
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-02 00:16:41 +0530

    added database name

--- backend/config/database.mjs
+++ backend/config/database.mjs
@@ -5,12 +5,12 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const name = "hostel-room-allotment";
 
-const url = `mongodb+srv://${username}:${password}@cluster0.tvkfzug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://${username}:${password}@cluster0.tvkfzug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/${name}`;
  
 mongoose.connect(url, {
   useUnifiedTopology: true
 }).then(() => {
-  console.log("Database connection is successful");
+  console.log("database connection is successful");
 }).catch((err) => {
-  console.log("Database connection failed: " + err);
+  console.log("database connection failed!\n" + err);
 });
