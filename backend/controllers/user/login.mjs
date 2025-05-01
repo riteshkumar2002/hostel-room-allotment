30559bb11be9dc85be75e39ac8c0fa68a57dec29
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-02 01:38:01 +0530

    implemented login

--- backend/controllers/user/login.mjs
+++ backend/controllers/user/login.mjs
@@ -1,3 +1,21 @@
-export default async function login() {
+import SignUp from "../../models/signup.js";
+
+export default async function login(req, res) {
+    try {
+        console.log(req.body);
+        const user = await SignUp.findOne({ adm_no: req.body.adm_no });
+        console.log(user);
+
+        if(user.password === req.body.password) {
+        res.status(200).json({ "status": "success" });
+
+        }
+        else {
+            res.status(401).json({"status": "failed", "message": "incorrect password"})
+        }
+    } catch(err) {
+        res.status(500).json({ "status": "failed", "message": err.message });
+    }
+
 
 }
