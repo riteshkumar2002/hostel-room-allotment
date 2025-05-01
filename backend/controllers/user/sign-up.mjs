cb10c0c73748721ff3deee769074b6b4a08f26d6
Ritesh Kumar <riteshdakshana123@gmail.com>
2025-05-02 01:37:02 +0530

    added sign up logic

--- backend/controllers/user/sign-up.mjs
+++ backend/controllers/user/sign-up.mjs
@@ -1,7 +1,20 @@
+import Signup from '../../models/signup.js'
+import applicableApplicants from '../../models/applicable-applicants.mjs';
+
 export default async (req, res) => {
     try {
-        const new_stu = await Signup.create(req.body);
-        res.status(201).json(new_stu);
+        const adm_no = req.body.adm_no;
+        
+        const found = await applicableApplicants.findOne({
+            applicableApplicants: { $in: [adm_no] }
+        });
+        
+        if(found) {
+            const new_stu = await Signup.create(req.body);
+            res.status(201).json(new_stu);
+        } else {
+            res.status(400).json({error: "You are not allowed for this hostel"});
+        }
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
