909cc16668cde710ccaa843d159da1e252d18837
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-02 00:06:25 +0530

    added routes

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,21 +1,17 @@
 // index.js
 import express from 'express';
-import adminRoutes from './routes/admin.js';
 import "./config/database.mjs";
+import routes from "./routes/index.mjs";
 // import userRoutes from './routers/user.js';
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-app.use('/admin', adminRoutes);
-// app.use('/user', userRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
