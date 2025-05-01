9ff69c1ecb3faa865604fc0d2a55483ff532e9c2
Ritesh Kumar <riteshdakshana123@gmail.com>
2025-05-02 00:34:12 +0530

    added applicable applicants

--- backend/routes/admin/index.mjs
+++ backend/routes/admin/index.mjs
@@ -1,13 +1,18 @@
 import { Router } from "express";
 
 import addConstraint from './add-constraint.mjs';
+import deleteConstraint from './delete-constraint.mjs'
+import getConstraints from './get-constraints.mjs'
+
 import approveAll from './approve-all.mjs'
 import approveAllocation from './approve-allocation.mjs'
-import deleteConstraint from './delete-constraint.mjs'
+
 import findNextRoom from './find-next-room.mjs'
-import getConstraints from './get-constraints.mjs'
+
+
 import allcateRoom from './allocate-room.mjs'
 import deallocateRoom from './deallocate-room.mjs'
+
 
 
 
