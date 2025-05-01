64324424b81944ffec6d006c745978fab4a5419a
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-02 02:34:03 +0530

    fixed database name

--- backend/config/database.mjs
+++ backend/config/database.mjs
@@ -5,7 +5,7 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const name = "hostel-room-allotment";
 
-const url = `mongodb+srv://${username}:${password}@cluster0.tvkfzug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/${name}`;
+const url = `mongodb+srv://${username}:${password}@cluster0.tvkfzug.mongodb.net/${name}?retryWrites=true&w=majority&appName=Cluster0`;
  
 mongoose.connect(url, {
   useUnifiedTopology: true
