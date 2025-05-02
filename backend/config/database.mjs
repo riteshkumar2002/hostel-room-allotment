1a0d4cf8d1c2cf8f149f27114d212416a015568c
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-02 18:11:06 +0530

    removed deprecated option!

--- backend/config/database.mjs
+++ backend/config/database.mjs
@@ -7,9 +7,7 @@ const name = "hostel-room-allotment";
 
 const url = `mongodb+srv://${username}:${password}@cluster0.tvkfzug.mongodb.net/${name}?retryWrites=true&w=majority&appName=Cluster0`;
  
-mongoose.connect(url, {
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(url).then(() => {
   console.log("database connection is successful");
 }).catch((err) => {
   console.log("database connection failed!\n" + err);
