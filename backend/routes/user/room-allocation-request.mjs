d69afe67d28dd6200dec4b03dd793433eeb1f8f9
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-03 20:16:37 +0530

    changed filename

--- backend/routes/user/room-allocation-request.mjs
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import controller from "../../controllers/user/room-allocation-request.mjs";
-
-const router = Router();
-router.post('/room-allocation-request', controller);
-
-export default router;
