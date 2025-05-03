66d86bf3fdfbee8eb4cba5d98a899bb5a1af8572
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-04 00:48:03 +0530

    implemented /approve-allocation

--- backend/controllers/admin/approve-allocation.mjs
+++ backend/controllers/admin/approve-allocation.mjs
@@ -1,3 +1,55 @@
-export default async(req,res)=>{
-    
+import mongoose from "mongoose";
+
+import Room from "../../models/room.js";
+import Allocation from "../../models/allocation.js";
+import AllocationRequest from "../../models/allocation-request.mjs";
+
+export default async(req, res) => {
+    const session = mongoose.startSession();
+
+    try {
+        const id = req.body._id;
+        const allocationRequest = await AllocationRequest.findOne({ _id: id });
+
+        if(allocationRequest == null) {
+            return res.status(404).json({ "status": "failed", "message": "allocationRequest not found" });
+        }
+
+        const rooms = await Room.find({ room_number: { $in: allocationRequest.room_numbers } });
+        const availabilities = new Map();
+
+        rooms.forEach(room => {
+            availabilities[room.room_number] = room.capacity - room.allocated_to.length;
+        });
+
+        console.log(availabilities);
+        console.log(rooms);
+
+        const roomNumbers = allocationRequest.room_numbers;
+
+        for(const roomNumber of roomNumbers) {
+            if(availabilities[roomNumber] > 0) {
+                session.startTransaction();
+
+                await Room.updateOne({ room_number: roomNumber }, { $push: {
+                    allocated_to: allocationRequest.admission_number
+                }}, { session });
+
+                await Allocation.create({
+                    admission_number: allocationRequest.admission_number, 
+                    room_number: allocationRequest.room_number
+                }, { session });
+
+                await session.commitTransaction();
+                break;
+            }
+        }
+
+        res.status(404).json({ "status": "failure", "message": "no vacant room available" });
+    } catch(err) {
+        await session.abortTransaction();
+        return res.status(500).json({ "status": "failed", "message": err.message });
+    } finally {
+        await session.endSession();
+    }
 }
--- backend/routes/admin/approve-allocation.mjs
+++ backend/routes/admin/approve-allocation.mjs
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import controller from '../../controllers/admin/approve-allocation.mjs'
+
 const router = Router();
+
+router.post("/approve", controller);
 
 export default router;
