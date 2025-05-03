220ff4edcea13e3da999233c8fc19aeae256baab
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-03 20:12:26 +0530

    added cors

--- backend/index.mjs
+++ backend/index.mjs
@@ -3,11 +3,13 @@ import express from 'express';
 import "./config/database.mjs";
 import routes from "./routes/index.mjs";
 import morgan from "morgan";
+import cors from "cors";
 // import userRoutes from './routers/user.js';
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(morgan("dev"))
 app.use(routes);
 
