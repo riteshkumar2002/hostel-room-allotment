5090b719bfc8aed01c889077bacccaf897198cbc
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-05-03 20:12:56 +0530

    changed allocation model

--- backend/models/allocation.js
+++ backend/models/allocation.js
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
 
 const AllocationSchema = new mongoose.Schema({
-  admission_no: {
+  admission_number: {
     type: String,
-    ref: 'SignUp', // Reference to 'admission_no' in SignUp collection
     required: true,
-    unique: true
+    unique: true,
+    index: true,
   },
   room_number: {
     type: String,
-    ref: 'Room', // Reference to 'room_no' in Room collection
-    required: true
+    required: true,
+    unique: true,
+    index: true,
   }
 });
 
